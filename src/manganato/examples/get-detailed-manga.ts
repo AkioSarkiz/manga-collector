c8306ec6081831f2380266c107e58c67a5690c7c
DLaptiev <akiosarkiz@gmail.com>
2024-07-18 22:35:20 +0200

    refactoring manganato source
    - removed node-html-parser package
    - add more tests
    - update examples
    - update error messages

--- src/manganato/examples/get-detailed-manga.ts
+++ src/manganato/examples/get-detailed-manga.ts
@@ -2,9 +2,11 @@ import { MangaScraperFactory, MangaSource } from "../../index.js";
 import fs from "fs";
 
 const main = async () => {
-  const scraper = await MangaScraperFactory.make(MangaSource.MANGANATO);
-  const result = await scraper.getDetailedManga("https://chapmanganato.to/manga-fy982633");
+  const url = "https://manganato.com/manga-gb984258";
   const filename = "detailed-manga.json";
+
+  const scraper = await MangaScraperFactory.make(MangaSource.MANGANATO);
+  const result = await scraper.getDetailedManga(url);
 
   await fs.promises.writeFile(filename, JSON.stringify(result, null, 2));
 
